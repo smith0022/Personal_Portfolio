74a2fd5267a5f05c9c9b564f0a5d9b7f8ef25ff8
smith0022 <mahantsmith@hotmail.com>
2023-07-20 16:31:49 +0530

    First commit

---
+++ src/data/user.js
@@ -0,0 +1,78 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Smith",
+		name: "Smith Mahant",
+		email: "mahantsmith@hotmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/MahantSmith",
+		github: "https://github.com/smith0022",
+		linkedin:
+			"https://www.linkedin.com/in/smith-mahant-37a8231ba?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVB2UO89zQOmwpI7tG54mUA%3D%3D",
+		instagram: "https://www.instagram.com/mahantsmith/",
+		stackoverflow: "https://stackoverflow.com/users/13351543/smith-mahant",
+		facebook: "https://www.facebook.com/smith.mahant.71/",
+	},
+
+	homepage: {
+		title: "A Computer science enthusiast ",
+		description:
+			"As a passionate and dedicated Computer Science enthusiast, I am currently pursuing my Bachelor's degree in Computer Science and Engineering at Nirma University Ahmedabad. With a strong foundation in programming and hands-on experience in Python, C, Java, and JavaScript, I am eager to contribute my skills and knowledge to the dynamic world of technology.",
+	},
+
+	about: {
+		title: "I’m Smith Mahant. I am currently pursuing B.tech in Computer Science in India.",
+		description:
+			"Throughout my academic journey, I have immersed myself in diverse projects and coursework that have honed my problem-solving abilities and fostered a keen eye for detail. My proficiency in Python, C, Java, and JavaScript has enabled me to develop robust and efficient applications, and I take great pride in crafting elegant and scalable solutions to complex challenges.Beyond the classroom, I have actively participated in hackathons and coding competitions, which have not only enhanced my technical acumen but also cultivated my teamwork and collaboration skills. I thrive in a collaborative environment where I can learn from and contribute to a diverse group of like-minded individuals.",
+	},
+	projects: [
+		{
+			title: "Gesture Volume Control",
+			description:
+				"i this project i used openCv for controlling the volume of a Linux system",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://gist.github.com/smith0022/bee798e790f3d3051375b3dbc79b6086",
+		},
+
+		{
+			title: " virtual painter",
+			description:
+				"it's a project that uses opencv for tracking your hand and storing those movements to create a sort of painting",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://gist.github.com/smith0022/7a68875e712408cb6bf51dbcfee94486",
+		},
+
+		{
+			title: "kivy",
+			description:
+				"I made an sanmple app using the python GUI library KivyMD",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://gist.github.com/smith0022/5077f4807ed54d458a89832a925cceb3",
+		},
+
+		{
+			title: "Object Oriented programming",
+			description:
+				"I made an example of object oriented programm in python",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://gist.github.com/smith0022/d05ff9398a2ba8a212cb11f5da5b44c3",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Things I’ve made trying to put my dent in the
+							universe.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+							During my academic journey and as a computer science
+							enthusiast, I have actively engaged in a diverse
+							range of projects that have allowed me to apply my
+							knowledge and technical skills to real-world
+							challenges. Each project has been an exciting
+							opportunity for me to explore different technologies
+							and problem domains, enabling me to grow both as a
+							developer and as a critical thinker. Here are some
+							of the key projects that I have worked on:
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
